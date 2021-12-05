95ca252dc6042798e1a307852d0ec705f3b3ee8a
Fatih Pense <fatihpense@gmail.com>
2021-12-05 09:25:54 +0300

    remove push parser, add namespace support and missing entities, add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# xml_sax [![crates.io](https://img.shields.io/crates/v/xml_sax.svg)](https://crates.io/crates/xml_sax)
+
+You don't need to add this library to your dependencies.
+
+Currently, only xml_oxide library implements this library and it is copied inside the library.
+
+After failed experiments I settled with a pull parser. If you want to use xml_sax interface to implement another parser we can discuss to improve the interface and options for having a single interface & multiple implementations.
--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,102 @@
-//old push
-pub trait SAXAttributes {
-    fn get_length(&self) -> usize;
-    // Need for boxing https://doc.rust-lang.org/book/trait-objects.html
-    fn iter(&self) -> Box<dyn Iterator<Item = Box<dyn SAXAttribute>>>;
-    // fn get_by_qualified_name(&mut self, index: usize) -> Option<Attribute>;
-    // when using trait as object use box otherwise:
-    // the trait `std::marker::Sized` is not implemented for `sax::SAXAttribute + 'static`
-    fn get_by_index(&self, index: usize) -> Option<Box<dyn SAXAttribute>>;
-}
+// Old Push API
+// pub trait SAXAttributes {
+//     fn get_length(&self) -> usize;
+//     // Need for boxing https://doc.rust-lang.org/book/trait-objects.html
+//     fn iter(&self) -> Box<dyn Iterator<Item = Box<dyn SAXAttribute>>>;
+//     // fn get_by_qualified_name(&mut self, index: usize) -> Option<Attribute>;
+//     // when using trait as object use box otherwise:
+//     // the trait `std::marker::Sized` is not implemented for `sax::SAXAttribute + 'static`
+//     fn get_by_index(&self, index: usize) -> Option<Box<dyn SAXAttribute>>;
+// }
 
-pub trait SAXAttribute {
-    // fn get_index() -> usize;
-    fn get_value(&self) -> &str;
-    fn get_local_name(&self) -> &str;
-    fn get_qualified_name(&self) -> &str;
-    fn get_uri(&self) -> &str;
-}
+// pub trait SAXAttribute {
+//     // fn get_index() -> usize;
+//     fn get_value(&self) -> &str;
+//     fn get_local_name(&self) -> &str;
+//     fn get_qualified_name(&self) -> &str;
+//     fn get_uri(&self) -> &str;
+// }
 
-pub trait ContentHandler {
-    fn start_document(&mut self);
-    fn end_document(&mut self);
+// pub trait ContentHandler {
+//     fn start_document(&mut self);
+//     fn end_document(&mut self);
 
-    fn start_element(
-        &mut self,
-        uri: &str,
-        local_name: &str,
-        qualified_name: &str,
-        attributes: &dyn SAXAttributes,
-    );
-    fn end_element(&mut self, uri: &str, local_name: &str, qualified_name: &str);
-    fn characters(&mut self, characters: &str);
+//     fn start_element(
+//         &mut self,
+//         uri: &str,
+//         local_name: &str,
+//         qualified_name: &str,
+//         attributes: &dyn SAXAttributes,
+//     );
+//     fn end_element(&mut self, uri: &str, local_name: &str, qualified_name: &str);
+//     fn characters(&mut self, characters: &str);
 
-    //fn start_prefix_mapping(&mut self, prefix: &str , uri: &str);
-    //fn end_prefix_mapping(&mut self, prefix: &str , uri: &str);
-}
+//     //fn start_prefix_mapping(&mut self, prefix: &str , uri: &str);
+//     //fn end_prefix_mapping(&mut self, prefix: &str , uri: &str);
+// }
 
-pub trait StatsHandler {
-    fn offset(&mut self, offset: usize);
-}
-
+// pub trait StatsHandler {
+//     fn offset(&mut self, offset: usize);
+// }
 //ErrorHandler
 
-// pull
+// Pull API
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Attribute<'a> {
     pub value: &'a str,
     pub name: &'a str,
-    //pub local_name: &'a str,
-
-    // fn get_value(&self) -> &str;
-    // fn get_local_name(&self) -> &str;
-    // fn get_qualified_name(&self) -> &str;
-    // fn get_uri(&self) -> &str;
+    // namespace aware
+    pub local_name: &'a str,
+    pub prefix: &'a str,
+    pub namespace: &'a str,
 }
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct StartElement<'a> {
     pub name: &'a str,
     pub attributes: Vec<Attribute<'a>>,
     pub is_empty: bool,
+    // namespace aware
+    pub local_name: &'a str,
+    pub prefix: &'a str,
+    pub namespace: &'a str,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct EndElement<'a> {
     pub name: &'a str,
+    // namespace aware
+    pub local_name: &'a str,
+    pub prefix: &'a str,
+    pub namespace: &'a str,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Reference<'a> {
     pub raw: &'a str,
-    pub resolved: &'a str,
+    pub resolved: Option<&'a str>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Event<'a> {
     StartDocument,
     EndDocument,
+
     StartElement(StartElement<'a>),
     EndElement(EndElement<'a>),
     Characters(&'a str),
     Reference(Reference<'a>),
+
     StartComment,
+    Comment(&'a str),
     EndComment,
-    StartCdata,
-    EndCdata,
-    // PI,
-    // DocType,
-    // Decl,
+
+    StartCdataSection,
+    Cdata(&'a str),
+    EndCdataSection,
+
+    DocumentTypeDeclaration(&'a str),
+    ProcessingInstruction(&'a str),
+    XmlDeclaration(&'a str),
+    Whitespace(&'a str),
 }
